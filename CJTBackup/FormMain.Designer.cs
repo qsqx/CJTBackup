ab5826200b2b966466f636fd16ff2ad2e63f4b11
qsqx <qs2388@yeah.net>
2019-10-04 16:51:24 +0800

    init project

---
+++ CJTBackup/FormMain.Designer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace CJTBackup
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.backupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backupToolStripMenuItem,
+            this.restoreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // backupToolStripMenuItem
+            // 
+            this.backupToolStripMenuItem.Name = "backupToolStripMenuItem";
+            this.backupToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
+            this.backupToolStripMenuItem.Text = "备份";
+            this.backupToolStripMenuItem.Click += new System.EventHandler(this.backupToolStripMenuItem_Click);
+            // 
+            // restoreToolStripMenuItem
+            // 
+            this.restoreToolStripMenuItem.Name = "restoreToolStripMenuItem";
+            this.restoreToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
+            this.restoreToolStripMenuItem.Text = "恢复";
+            this.restoreToolStripMenuItem.Click += new System.EventHandler(this.restoreToolStripMenuItem_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(0, 35);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(800, 416);
+            this.logTextBox.TabIndex = 1;
+            this.logTextBox.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "畅捷通T3备份工具";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem backupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restoreToolStripMenuItem;
+
+        private Mgr.Logger GetLogger()
+        {
+            return (text) => {
+                Action action = new Action(() => {
+                    logTextBox.AppendText(text);
+                });
+
+                if (logTextBox.InvokeRequired)
+                {
+                    logTextBox.Invoke(action);
+                }
+                else
+                {
+                    action.Invoke();
+                }
+            };
+        }
+
+        private void backupToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            System.Windows.Forms.SaveFileDialog path = new System.Windows.Forms.SaveFileDialog();
+            path.Filter = "ZIP压缩包|*.zip";
+            path.ShowDialog();
+
+            if (path.FileName == "")
+                return;
+
+            Mgr mgr = new Mgr(GetLogger());
+            new System.Threading.Thread(() => { mgr.Backup(path.FileName); }).Start();
+        }
+
+        private void restoreToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog path = new System.Windows.Forms.OpenFileDialog();
+            path.Filter = "ZIP压缩包|*.zip";
+            path.ShowDialog();
+            if (path.FileName == "")
+                return;
+
+            Mgr mgr = new Mgr(GetLogger());
+            new System.Threading.Thread(() => { mgr.Restore(path.FileName); }).Start();
+        }
+
+        private System.Windows.Forms.RichTextBox logTextBox;
+    }
+}
+
---
+++ CJTBackup/Mgr.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CJTBackup
+{
+    class Mgr
+    {
+        public delegate void Logger(string text);
+
+        private static string defaultConnectCfg = "data source=.;user id=sa;pwd=sa";
+
+        private Logger logger;
+
+        public Mgr(Logger logger)
+        {
+            this.logger = logger;
+        }
+        private string GetTempDir()
+        {
+            string tmp = Path.GetTempPath();
+
+            return (tmp.EndsWith(@"\") ? tmp : tmp + @"\") + @"CJTBackup\" + Guid.NewGuid().ToString();
+        }
+
+        private void createDir(string dir)
+        {
+            DirectorySecurity rules = new DirectorySecurity();
+            rules.AddAccessRule(new FileSystemAccessRule("Authenticated Users", FileSystemRights.Modify, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+            rules.AddAccessRule(new FileSystemAccessRule("Users", FileSystemRights.ReadAndExecute, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+            Directory.CreateDirectory(dir, rules);
+        }
+        private void createTmpDir(string tmpdir)
+        {
+            logger("Create tmpdir " + tmpdir + " ... ");
+            createDir(tmpdir);
+            logger("Success\n");
+        }
+
+        private void deleteTmpDir(string tmpdir)
+        {
+            logger("Clear tmpdir ... ");
+            Directory.Delete(tmpdir, true);
+            logger("Success\n");
+        }
+
+        private string[] QueryDatabases(SqlConnection conn)
+        {
+            List<string> dbs = new List<string>();
+
+            using (SqlCommand stmt = new SqlCommand("select name from sys.databases where database_id > 4", conn))
+            {
+                SqlDataReader rows = stmt.ExecuteReader();
+                while (rows.Read())
+                {
+                    dbs.Add(rows[0].ToString());
+                }
+                rows.Close();
+            }
+            return dbs.ToArray();
+        }
+        private void doBackup(ZipArchive archive, string tmpdir)
+        {
+            using (SqlConnection conn = new SqlConnection(defaultConnectCfg))
+            {
+                conn.Open();
+                string[] dbs = QueryDatabases(conn);
+
+
+                foreach (string db in dbs)
+                {
+                    if (db == "UFData_998_2012" || db == "UFData_999_2011")
+                        continue;
+
+                    logger("Backup Database " + db + " ... ");
+                    string path = tmpdir + @"\" + db + ".bak";
+
+                    using (SqlCommand stmt = conn.CreateCommand())
+                    {
+                        stmt.CommandText = "BACKUP DATABASE [" + db + "] TO DISK = @path";
+  
+                        stmt.Parameters.AddWithValue("@path", path);
+                        stmt.ExecuteNonQuery();
+
+                        archive.CreateEntryFromFile(path, db + ".bak");
+                    }
+                    logger("Success\n");
+                }
+            }
+        }
+
+        public int Backup(string path)
+        {
+            string tmpdir = GetTempDir();
+
+            try
+            {
+                using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Create))
+                {
+                    createTmpDir(tmpdir);
+                    doBackup(archive, tmpdir);
+                    logger("Pack ZIP File ...");
+                }
+                logger("success\n");
+                deleteTmpDir(tmpdir);
+                logger("备份成功\n");
+            } catch (Exception e)
+            {
+                logger(e.ToString());
+                logger("\n备份失败\n");
+            }
+            return 0;
+        }
+
+        private void doRestore(string tmpdir)
+        {
+            string[] files = Directory.GetFiles(tmpdir, "*.bak");
+
+            using (SqlConnection conn = new SqlConnection(defaultConnectCfg))
+            {
+                conn.Open();
+                string[] dbs = QueryDatabases(conn);
+
+                foreach (string filepath in files)
+                {
+                    string filename = Path.GetFileName(filepath);
+                    string db = filename.Split('.')[0];
+
+                    if (dbs.Contains(db))
+                    {
+                        logger("Restore Database " + db + " ... ");
+                        using (SqlCommand stmt = conn.CreateCommand())
+                        {
+                            stmt.CommandText = "RESTORE DATABASE [" + db + "] FROM DISK = @path WITH RECOVERY";
+                            stmt.Parameters.AddWithValue("@path", filepath);
+                            stmt.ExecuteNonQuery();
+                        }
+                        logger(" Success\n");
+                    } else
+                    {
+                        logger("Create Database " + db + " From Backup ... ");
+                        string[] splited = db.Split('_');
+                        if (splited.Length != 3)
+                            throw new Exception("Invalid Database Name");
+                        string dbdir = @"C:\UFSMART\Admin\ZT" + splited[1] + @"\" + splited[2];
+                        createDir(dbdir);
+                        string dataname = "", logname = "";
+                        using (SqlCommand stmt = conn.CreateCommand())
+                        {
+                            stmt.CommandText = "RESTORE FILELISTONLY FROM DISK = @path";
+                            stmt.Parameters.AddWithValue("@path", filepath);
+                            SqlDataReader rows = stmt.ExecuteReader();
+                            while (rows.Read()){
+                                string logicalName = rows[0].ToString();
+                                string typ = rows[2].ToString();
+                                if (typ == "D")
+                                    dataname = logicalName;
+                                else if (typ == "L")
+                                    logname = logicalName;
+                            }
+                            rows.Close();
+                        }
+                        if (logname == "" || dataname == "")
+                            throw new Exception("invalid backup file");
+                        using (SqlCommand stmt = conn.CreateCommand())
+                        {
+                            stmt.CommandText = "RESTORE DATABASE " + db + " FROM DISK = @path WITH RECOVERY, MOVE @dataname TO @datafile, MOVE @logname TO @logfile";
+                            stmt.Parameters.AddWithValue("@path", filepath);
+                            stmt.Parameters.AddWithValue("@dataname", dataname);
+                            stmt.Parameters.AddWithValue("@datafile", dbdir + @"\UFDATA.mdf");
+                            stmt.Parameters.AddWithValue("@logname", logname);
+                            stmt.Parameters.AddWithValue("@logfile", dbdir + @"\UFDATA.ldf");
+                            stmt.ExecuteNonQuery();
+                        }
+                        logger(" Success\n");
+                    }
+                }
+            }
+        }
+
+
+        public int Restore(string path)
+        {
+            string tmpdir = GetTempDir();
+
+            try
+            {
+                using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Read))
+                {
+                    createTmpDir(tmpdir);
+                    logger("Extact Files ... ");
+                    archive.ExtractToDirectory(tmpdir);
+                    logger("Success\n");
+                }
+                doRestore(tmpdir);
+                deleteTmpDir(tmpdir);
+                logger("恢复成功\n");
+            }
+            catch (Exception e)
+            {
+                logger(e.ToString());
+                logger("\n恢复失败\n");
+            }
+
+            return 0;
+        }
+    }
+}
